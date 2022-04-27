61d4289f668164db14186697dcd27f3f024054b9
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-04-27 07:10:33 -0300

    Create contract.js

---
+++ contract.js
@@ -0,0 +1,10 @@
+{
+"entity": {
+"domain": "exordium.co"
+},
+"issuer_pubkey": "030cff26f9c0d365f090e24917277e23269d7ef5d7f06dec9f09de9255b8950208",
+"name": "EXO Token Europe",
+"precision": 0,
+"ticker": "EXOeu",
+"version": 0
+}
